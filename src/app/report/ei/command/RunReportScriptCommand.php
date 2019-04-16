ffd419f2409785764c36a18fdcd379ba11af80a9
Andreas von Burg <a@von-burg.net>
2019-04-16 19:24:55 +0200

    rocket 2.1 implementations

--- src/app/report/ei/command/RunReportScriptCommand.php
+++ src/app/report/ei/command/RunReportScriptCommand.php
@@ -8,14 +8,12 @@ use n2n\impl\web\ui\view\html\HtmlView;
 use n2n\util\uri\Path;
 use n2n\l10n\N2nLocale;
 use n2n\web\http\controller\Controller;
-use rocket\ei\component\command\control\EntryControlComponent;
 use rocket\ei\util\Eiu;
 use n2n\core\container\N2nContext;
 use rocket\si\control\SiButton;
 use rocket\si\control\SiIconType;
-use rocket\ei\manage\control\HrefControl;
 
-class RunReportScriptCommand extends IndependentEiCommandAdapter implements EntryControlComponent {
+class RunReportScriptCommand extends IndependentEiCommandAdapter {
 	const DEFAULT_ID = 'run-report';
 	const CONTROL_DETAIL_KEY = 'report';
 	
@@ -42,20 +40,14 @@ class RunReportScriptCommand extends IndependentEiCommandAdapter implements Entr
 		return $eiu->lookup(RunReportController::class);
 	}
 
-	/**
-	 * {@inheritDoc}
-	 * @see \rocket\ei\component\command\control\EntryControlComponent::getEntryControlOptions()
-	 */
+
 	public function getEntryControlOptions(N2nContext $n2nContext, N2nLocale $n2nLocale): array {
 		$dtc = new DynamicTextCollection('report', $n2nLocale);
 		return array(self::DEFAULT_ID => $dtc->translate('script_cmd_export_report_label'));
 	}
 	
-	/**
-	 * {@inheritDoc}
-	 * @see \rocket\ei\component\command\control\EntryControlComponent::createEntryControls()
-	 */
-	public function createEntryControls(Eiu $eiu, HtmlView $view): array {
+
+	public function createEntryControls(Eiu $eiu): array {
 		$eiuFrame = $eiu->frame();
 		$siButton = new SiButton(
 				$view->getL10nText('script_cmd_run_report_label', null, null, null, 'report'), 
